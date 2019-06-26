87f3014ba2835dba0419edc7a1c1d5258883b1e3
MohamedAnwerEida <01200643888r@gmail.com>
2019-06-26 12:12:40 +0200

    1.0 v

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format npm install --save-dev eslint-config-rallycoding */
+
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
